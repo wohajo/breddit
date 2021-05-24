6efd1c89d98d67cf456e54b680a680525b7868df
Michał Prawda <46087735+wohajo@users.noreply.github.com>
2021-05-24 19:27:23 +0200

    Initial server commit

---
+++ breddit-server/index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+let server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World!\n");
+});
+
+server.listen(3000, "127.0.0.1");
+console.log("Node server running on port 3000");
